185e85529b8ca9b346fd722a1b321fa5108385df
Maksym Nezhurin <maksym.nezhurin@wework.com>
2024-01-31 13:05:26 +0100

    Implement font family for heading elements

--- components/hTag/hTag.tsx
+++ components/hTag/hTag.tsx
@@ -20,19 +20,19 @@ interface hTagProps {
     style?: object,
 }
 
-const jose = Josefin_Sans({ subsets: ['latin'] });
+const jose = Josefin_Sans({ subsets: ['latin'], variable: '--font-jose' });
 
 export const HTag = ({ tag, children, className, ...props }: hTagProps): ReactElement => {
 
     switch (tag) {
         case 'h1':
-            return <h1 className={cn(styles.h1, className, jose.className)} {...props}>{children}</h1>
+            return <h1 className={cn(styles.h1, className, jose.variable)} {...props}>{children}</h1>
         case 'h2':
-            return <h2 className={cn(styles.h2, className, jose.className)} {...props}>{children}</h2>
+            return <h2 className={cn(styles.h2, className, jose.variable)} {...props}>{children}</h2>
         case 'h3':
-            return <h3 className={cn(styles.h3, className, jose.className)} {...props}>{children}</h3>
+            return <h3 className={cn(styles.h3, className, jose.variable)} {...props}>{children}</h3>
         case 'h4':
-            return <h4 className={cn(styles.h4, className, jose.className)} {...props}>{children}</h4>
+            return <h4 className={cn(styles.h4, className, jose.variable)} {...props}>{children}</h4>
         default:
             return <span>{children}</span>
     }
